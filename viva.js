bb5f42e01744d125acf63475f012ef6062a3173d
Mateus-Az <mateo.azv3@gmail.com>
2023-08-25 20:18:38 -0300

    day 3

--- viva.js
+++ viva.js
@@ -7,4 +7,13 @@ function teste() {
   console.log(a + c);
 }
 
+function teste2(num){
+  console.log("========");
+  if(num >= 30 && num % 2 == 0){
+    return "maior que 30 e par";
+  }
+}
+
+
 teste();
+console.log(teste2(30));
